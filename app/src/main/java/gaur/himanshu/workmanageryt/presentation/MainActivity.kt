da6c2230abe71778b123ea881c0a139620a541a6
Ankitgujare <ankitgujare001@gmail.com>
2026-08-16 00:26:15 +0530

    Added User Interface

--- app/src/main/java/gaur/himanshu/workmanageryt/presentation/MainActivity.kt
+++ app/src/main/java/gaur/himanshu/workmanageryt/presentation/MainActivity.kt
@@ -4,23 +4,56 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MonotonicFrameClock
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 import gaur.himanshu.workmanageryt.data.local.QuoteDatabase
 import gaur.himanshu.workmanageryt.domain.model.Quote
 import gaur.himanshu.workmanageryt.presentation.ui.theme.WorkManagerYTTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewmodel = hiltViewModel<MainViewModel>()
             WorkManagerYTTheme {
                 Surface(modifier = Modifier.safeContentPadding()) {
-                    MainScreen()
+                    MainScreen(viewmodel){
+                        viewmodel.getQuote()
+                    }
 
                 }
             }
@@ -29,8 +62,60 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen(viewModel: MainViewModel,fetchQuote:()->Unit) {
+    val uistate by viewModel.uistate.collectAsState()
 
+    Scaffold(
+        topBar = { TopAppBar(title ={Text("Quote's")},
+           actions = {
+               IconButton(onClick = fetchQuote) {
+                   Icon(imageVector =Icons.Default.Refresh, contentDescription ="icon refresh")
+               }
+           }
+
+
+        ) }
+
+    ) {
+
+        if (uistate.data.isEmpty()){
+            Box(modifier=Modifier.padding(it).fillMaxSize(), contentAlignment = Alignment.Center){
+                Text("No Item Found")
+            }
+
+        }else{
+            LazyColumn(
+                modifier=Modifier.padding(it).fillMaxSize()
+            ) {
+                items(uistate.data){
+                    Card(
+                        modifier = Modifier.padding(8.dp).fillMaxWidth()
+                    ) {
+                        Column(modifier=Modifier.padding(8.dp)){
+                            Text(text = it.quote)
+                            Spacer(modifier=Modifier.height(8.dp))
+                            Text(text ="Author : ${it.author}" )
+                            Spacer(modifier=Modifier.height(8.dp))
+                            Row(
+                                modifier=Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text(text = it.time.toString())
+                                Text(text = it.workType)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+    }
 }
 
